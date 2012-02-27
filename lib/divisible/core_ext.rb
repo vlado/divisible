301338725d3dbf0a219631a36594aea65a4cdab2
Hrvoje Šimić <shime.ferovac@gmail.com>
2012-02-27 22:21:59 +0100

    check for multiple arguments, added tests

--- lib/divisible/core_ext.rb
+++ lib/divisible/core_ext.rb
@@ -1,7 +1,9 @@
 class Fixnum
-  
-  def divisible_by(d)
-    Divisible.check(self, d)
+  def divisible_by?(first,*args)
+    args += [first]
+    args.each do |arg|
+      return false unless Divisible.check(self, arg)
+    end
+    true
   end
-  
 end
---
+++ test/test_divisible.rb
@@ -0,0 +1,41 @@
+require "test/unit"
+require "divisible"
+require "divisible/core_ext"
+
+
+class TestSomething < Test::Unit::TestCase
+  def test_if_loaded
+    assert Fixnum.method_defined?(:divisible_by?)
+  end
+
+  def check_module_call
+    assert Divisible.check(4,2)
+  end
+  
+  def test_simple
+    assert 2.divisible_by?(2)
+  end
+
+  def test_complex
+    assert 100.divisible_by?(2,4,5,10,20,25,50)
+  end
+
+  def test_complex_with_zero
+    assert_raise ZeroDivisionError do
+      assert_equal false, 100.divisible_by?(2,4,5,10,20,25,50,0)
+    end
+  end
+
+  def test_blank
+    assert_raise ArgumentError do
+      2.divisible_by?
+    end
+  end
+
+  def test_division_by_zero
+    assert_raise ZeroDivisionError do
+      assert_equal false, 2.divisible_by?(0)
+    end
+  end
+
+end
